7338366868cfd7ab2c40842ed08f6299957233eb
mufanx <xmfnnj@outlook.com>
2021-05-23 12:40:05 +0800

    注释

--- ic3/pdr.py
+++ ic3/pdr.py
@@ -11,7 +11,7 @@ class tCube:
     def addModel(self, lMap, model):
         no_primes = [l for l in model if str(l)[-1] != '\'']
         no_input = [l for l in no_primes if str(l)[0] != 'i']
-        self.cubeLiterals.extend([lMap[str(l)] == model[l] for l in no_input])
+        self.add(simplify(And([lMap[str(l)] == model[l] for l in no_input])))
 
     def addAnds(self, ms):
         self.cubeLiterals.extend(ms)
@@ -88,6 +88,7 @@ class PDR:
             c = self.getBadCube()
             if c is not None:
                 print("get bad cube!")
+                print(c)
                 trace = self.recBlockCube(c)
                 if trace is not None:
                     print("Found trace ending in bad state:")
@@ -104,7 +105,7 @@ class PDR:
                         s = Solver()
                         s.add(fi.cube())
                         s.add(self.trans.cube())
-                        s.add(Not(substitute(c, self.primeMap)))
+                        s.add(Not(substitute(c, self.primeMap)))  # F[i] and T and Not(c)'
                         if s.check() == unsat:
                             self.R[index + 1].add(c)
                     if self.checkForInduction(fi):
@@ -116,7 +117,7 @@ class PDR:
         s = Solver()
         s.add(self.trans.cube())
         s.add(frame.cube())
-        s.add(Not(substitute(frame.cube(), self.primeMap)))
+        s.add(Not(substitute(frame.cube(), self.primeMap)))  # T and F[i] and Not(F[i])'
         if s.check() == unsat:
             return True
         return False
@@ -139,16 +140,22 @@ class PDR:
         return None
 
     def MIC(self, q: tCube):
-        i = 0
-        while True:
-            if i < len(q.cubeLiterals) - 1:
-                i = i + 1
-            else:
-                break
+        for i in range(len(q.cubeLiterals)):
             q1 = q.delete(i)
             if self.down(q1):
                 q = q1
         return q
+        # i = 0
+        # while True:
+        #     print(i)
+        #     if i < len(q.cubeLiterals) - 1:
+        #         i += 1
+        #     else:
+        #         break
+        #     q1 = q.delete(i)
+        #     if self.down(q1):
+        #         q = q1
+        # return q
 
     def down(self, q: tCube):
         while True:
@@ -163,138 +170,33 @@ class PDR:
                       substitute(q.cube(), self.primeMap)))  # Fi and not(q) and T and q'
             if unsat == s.check():
                 return True
-            # q.addModel(self.lMap, s.model())
+            # m = s.model()
+            # q.addModel(self.lMap, m)
             # s.pop()
             return False
 
-    def generalize_iter(self, c: tClause):
-        done = False
-        for i in range(1, 10):
-            if done:
-                break
-            done = True
-            for j in range(len(c.clauseLiterals)):
-                g = c.delete(j)
-                s = Solver()
-                s.push()
-                s.add(self.R[0].cube())
-                s.add(Not(g.clause()))
-                if s.check() == sat:
-                    s.pop()
-                    continue
-                s.pop()
-                s.push()
-                s.add(And(self.R[c.t].cube(), self.trans.cube(), g.clause()), substitute(g.clause(), self.primeMap))
-                if s.check() == sat:
-                    cc = tClause(c.t)
-                    while s.check() == sat:
-                        c0 = tCube()
-                        c0.addModel(self.lMap, s.model())
-                        cc.add(c0.cube())
-                        s.add(Not(c0.cube()))
-                    s.pop()
-                    s.push()
-                    s.add(And(cc.clause(), self.R[0].cube()))
-                    print(1)
-                    s0 = Solver()
-                    s0.add(Or(cc.clause(), g.clause()))
-                    s0.add(self.R[0].cube())
-                    if unsat == s0.check():
-                        print("Yyyyys")
-                        exit(1)
-                    else:
-                        print("Nooo")
-                        exit(1)
-
-                    while s.check() == sat:
-                        print(2)
-                        s1 = Solver()
-                        s1.push()
-                        for l in g.clauseLiterals:
-                            s1.add(cc.clause())
-                            s1.add(l)
-                            if unsat == s1:
-                                cc.add(l)
-                                break
-                        s.pop()
-                        s.push()
-                        s.add(And(cc.clause(), self.R[0].cube()))
-                    c = cc
-                    done = False
-                    break
-                else:
-                    s.pop()
-
-    def generalize_CTG(self, c: tClause, rec_lvl=1):
-        required = []
-        fail = 0
-        for l in c.clauseLiterals:
-            cand = c.delete(l)
-            if self.down_CTG(cand, rec_lvl, required):
-                c = cand
-                fail = 0
-            else:
-                if ++fail > 5:
-                    break
-                required.append(l)
-
-    def down_CTG(self, c: tClause, rec_lvl, required: []):
-        ctgs = 0
-        s = Solver()
-        while True:
-            s.push()
-            s.add(And(self.R[0].cube(), Not(c.clause())))
-            if s.check() == sat:
-                return False
-            s.pop()
-            s.push()
-            s.add(And(self.R[c.t].cube(), self.trans.cube(), c.clause()), substitute(c.clause(), *self.primeMap))
-            if s.check() == sat:
-                cc = tClause(c.t)
-                while s.check() == sat:
-                    c0 = tCube()
-                    c0.addModel(self.lMap, s.model())
-                    cc.add(c0)
-                    s.add(Not(s.model()))
-                s.pop()
-                s.push()
-                s.add(And(cc.clause(), self.R[0].cube()))
-                while s.check() == sat:
-                    s1 = Solver()
-                    s1.push()
-                    for l in c.clauseLiterals:
-                        s1.add(cc.clause())
-                        s1.add(l)
-                        if unsat == s1:
-                            cc.add(l)
-                            break
-                    s.pop()
-                    s.push()
-                    s.add(And(cc.clause(), self.R[0].cube()))
-                c = cc
-                return True
-            elif rec_lvl > 4:
-                return False
-            else:
-                pass
-                # s = self.get_predecessor(c.t, Not(substitute(c, *self.primeMap))
-
-    def get_predecessor(self, c: tCube) -> tCube:
-        s = Solver()
-        s.add(And(self.R[c.t - 1].cube(), self.trans.cube(), Not(c.cube()), substitute(c.cube(), self.primeMap)))
-        assert sat == s.check()
-        model = s.model()
-        c0 = tCube(c.t - 1)
-        inputs = [self.lMap[str(l)] == model[l] for l in model if str(l)[0] == 'i']
-        p = [self.lMap[str(l)] == model[l] for l in model if str(l)[0] == 'v']
-        c0.addAnds(p)
-        return c0
-        # for iter in range(0, max_iter):
-        #     s0 = Solver()
-        #     s0.add(And(self.trans.cube(), inputs, substitute(c, self.primeMap), p))
-        #     ms = tCube()
-        #     while sat == s0.check():
-        #         m = s0.model()
+    # def tcgMIC(self, q: tCube, d: int):
+    #     for i in range(len(q.cubeLiterals)):
+    #         q1 = q.delete(i)
+    #         if self.ctgDown(q1, d):
+    #             q = q1
+    #     return q
+    #
+    # def ctgDown(self, q: tCube, d: int):
+    #     ctgs = 0
+    #     while True:
+    #         s = Solver()
+    #         s.push()
+    #         s.add(And(self.R[0].cube(), Not(q.cube())))
+    #         if unsat == s.check():
+    #             return False
+    #         s.pop()
+    #         s.push()
+    #         s.add(And(self.R[q.t].cube(), Not(q.cube()), self.trans.cube(),
+    #                   substitute(q.cube(), self.primeMap)))  # Fi and not(q) and T and q'
+    #         if unsat == s.check():
+    #             return True
+    #         m = s.model()
 
     # tcube is bad state
     def solveRelative(self, tcube):
@@ -303,22 +205,22 @@ class PDR:
         s.add(self.R[tcube.t - 1].cube())
         s.add(self.trans.cube())
         s.add(Not(tcube.cube()))
-        s.add(cubePrime)
+        s.add(cubePrime)  # F[i - 1] and T and Not(badCube) and badCube'
         if s.check() == sat:
             model = s.model()
             c = tCube(tcube.t - 1)
-            c.addModel(self.lMap, model)
+            c.addModel(self.lMap, model)  # c = sat_model
             return c
         return None
 
     def getBadCube(self):
         print("seek for bad cube...")
-        model = And(Not(self.post.cube()), self.R[-1].cube())
+        model = And(Not(self.post.cube()), self.R[-1].cube())  # F[k] and Not(p)
         s = Solver()
         s.add(model)
         if s.check() == sat:
             res = tCube(len(self.R) - 1)
-            res.addModel(self.lMap, s.model())
+            res.addModel(self.lMap, s.model())  # res = sat_model
             return res
         else:
             return None
